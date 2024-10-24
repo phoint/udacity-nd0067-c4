81f7e9cc3daae6cdfd585355ad4242b0776dd42a
root <phoi.nt2806@gmail.com>
2024-10-24 14:46:42 +0000

    Initial Commit

---
+++ .ipynb_checkpoints/setup_node-checkpoint.sh
@@ -0,0 +1,4 @@
+export NVM_DIR=/opt/FSJSND/.nvm
+cd $NVM_DIR && . nvm.sh && cd -
+
+alias aws=/opt/FSJSND/aws_cli/v2/current/bin/aws
---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=myPassword
+export POSTGRES_HOST=mydbinstance.c7ta24ek99hw.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-751397240855
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
